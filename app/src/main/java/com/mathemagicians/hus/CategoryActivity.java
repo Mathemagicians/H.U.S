26fc7f1e09a4cf838c8f9c74bfad7aae66fb42a7
Rajat Goyal <rajatgoyal715@gmail.com>
2017-01-20 23:45:12 +0530

    Activities linked

--- app/src/main/java/com/mathemagicians/hus/CategoryActivity.java
+++ app/src/main/java/com/mathemagicians/hus/CategoryActivity.java
@@ -1,5 +1,6 @@
 package com.mathemagicians.hus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,6 +20,19 @@ public class CategoryActivity extends AppCompatActivity {
     }
 
     public void next(View view) {
-
+        switch(category.getCheckedRadioButtonId()) {
+            case R.id.cate1:
+                Intent i1 = new Intent(CategoryActivity.this, FormEmployer.class);
+                startActivity(i1);
+                finish();
+            case R.id.cate2:
+                Intent i2 = new Intent(CategoryActivity.this, FormEmployee.class);
+                startActivity(i2);
+                finish();
+            case R.id.cate3:
+                Intent i3 = new Intent(CategoryActivity.this, MainActivity.class);
+                startActivity(i3);
+                finish();
+        }
     }
 }
